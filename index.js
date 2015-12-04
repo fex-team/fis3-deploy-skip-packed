0680d1806f19635c1fe348d338da2ee4a74d9ed0
2betop <liaoxuezhi@icloud.com>
2015-12-04 12:14:53 +0800

    bugfix

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ module.exports = function(options, modified, total, callback) {
   var file;
 
   while ((file = modified[i--])) {
-    if (file.map && file.map.pkg) {
+    if (file.map && (file.map.pkg || file.map.aioPkg || file.map.cssspritePkg)) {
       modified.splice(i + 1, 1);
     }
   }
 
   i = total.length;
   while ((file = total[i--])) {
-    if (file.map && file.map.pkg) {
+    if (file.map && (file.map.pkg || file.map.aioPkg || file.map.cssspritePkg)) {
       total.splice(i + 1, 1);
     }
   }
