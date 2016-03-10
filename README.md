3068cee8b0d2b62ff4978173fcfc41b93f5542b1
2betop <liaoxuezhi@icloud.com>
2016-03-10 17:07:41 +0800

    update

--- README.md
+++ README.md
@@ -8,7 +8,9 @@ fis3-deploy-skip-packed
 ```js
 fis.match('**', {
   deploy: [
-    fis.plugin('skip-packed'),
+    fis.plugin('skip-packed', {
+      // 配置项
+    }),
 
     fis.plugin('local-deliver', {
       to: 'output'
@@ -19,3 +21,10 @@ fis.match('**', {
 
 * 升级 fis3-postpackager-loader 到最新版本，如果你希望过滤掉被其 All In One 的资源。
 * 升级最新的 fis3 如果你希望过滤掉已被  css sprite 了小图片。
+
+## 配置项说明
+
+* `skipPackedToPkg` 默认 `true` 是否过滤掉被打包的文件
+* `skipPackedToAIO` 默认 `true` 是否过滤掉被 AllInOne 打包的文件
+* `skipPackedToCssSprite` 默认为 `true` 是否过滤掉被 css sprite 合并的文件。
+* `ignore` 即便设置了过滤，但是如果目标文件 ignore 列表中，还是不会被过滤。glob 语法，数组或者字符串。
--- index.js
+++ index.js
@@ -4,21 +4,76 @@
  */
 module.exports = function(options, modified, total, callback) {
 
+  var ignore = options.ignore || [];
+
+  if (typeof ignore === 'string') {
+    ignore = ignore.split(/[\s,]+/);
+  } else if (ignore && !Array.isArray(ignore)) {
+    ignore = [ignore];
+  }
+
+  ignore = ignore && ignore.map(function(str) {
+    return fis.util.glob(str);
+  });
+
+  var isIgnored = function(path) {
+    if (!ignore) {
+      return false;
+    }
+
+    var allPassed = ignore.every(function(pattern) {
+      if (pattern.test(path)) {
+        return false;
+      }
+
+      return true;
+    });
+
+    return !allPassed;
+  }
+
   var i = modified.length - 1;
   var file;
 
   while ((file = modified[i--])) {
-    if (file.map && (file.map.pkg || file.map.aioPkg || file.map.cssspritePkg)) {
+    if (file.map && (
+        options.skipPackedToPkg && file.map.pkg ||
+        options.skipPackedToAIO && file.map.aioPkg ||
+        options.skipPackedToCssSprite && file.map.cssspritePkg)) {
+
+      // 检查是否 ignore 了
+      if (ignore && isIgnored(file.subpath)) {
+        continue;
+      }
+
       modified.splice(i + 1, 1);
     }
   }
 
   i = total.length - 1;
   while ((file = total[i--])) {
-    if (file.map && (file.map.pkg || file.map.aioPkg || file.map.cssspritePkg)) {
+    if (file.map && (
+        options.skipPackedToPkg && file.map.pkg ||
+        options.skipPackedToAIO && file.map.aioPkg ||
+        options.skipPackedToCssSprite && file.map.cssspritePkg)) {
+
+      // 检查是否 ignore 了
+      if (ignore && isIgnored(file.subpath)) {
+        console.log(file.subpath);
+        continue;
+      }
+
       total.splice(i + 1, 1);
     }
   }
 
   callback();
+};
+
+module.exports.options = {
+  skipPackedToPkg: true,
+  skipPackedToAIO: true,
+  skipPackedToCssSprite: true,
+  ignore: null
+  // ignore: ['/src/**']
 };
